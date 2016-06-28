d345c28fbc6c5cd69ebadaaf1f0bc8bbc08df9c6
David Amaro-Alcalá <dav1494@ciencias.unam.mx>
2016-06-27 22:37:10 -0500

    Ahora lapiz nos devuelve los estados en la base computacional.

--- lib.m
+++ lib.m
@@ -71,7 +71,8 @@ Mexico[k_, q_, M_, j_, m_] := Module[{l = Length[M], prod = 0, i},
    ClebschGordan[{1/2, M[[1]]}, {k[[l - 2]], q[[l - 2]]}, {j, m}]]
 
 Lapiz[K_, M_, j_, m_] := If[Total[M] == m && QVD[K, GeneradorQs[M]],
-  Mexico[K, GeneradorQs[M], M, j, m]*EspinAEstado[M], 0]
+  Mexico[K, GeneradorQs[M], M, j, m]*EspinAEstado[M],
+  Table[0,{i,2^Length[M]}]]
 
 Estados[n_]:=Tuples[{1/2,-1/2},{n}]
 
